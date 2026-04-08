25bc1b0206d5382d6848b23f0b48c7c1de0c437d
Sarah Syeda <98742887+Sarah2211@users.noreply.github.com>
2026-04-08 09:18:59 +0400

    Add Lambda function to get notes from DynamoDB

---
+++ Project _6 - AWS Serverless Notes App/Lambda/getNote.py
@@ -0,0 +1,18 @@
+import json
+import boto3
+
+dynamodb = boto3.resource('dynamodb')
+table = dynamodb.Table('NotesApp')
+
+def lambda_handler(event, context):
+    response = table.scan()
+
+    return {
+        'statusCode': 200,
+        'headers': {
+            'Access-Control-Allow-Origin': '*',
+            'Access-Control-Allow-Headers': '*',
+            'Access-Control-Allow-Methods': '*'
+        },
+        'body': json.dumps(response['Items'])
+    }
